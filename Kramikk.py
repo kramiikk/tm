1ed5163bd116a11120f34531a07c3dde4ccddbca
Fopregh <hifund@yandex.ru>
2021-11-26 00:10:05 +0600

    zzz

--- Kramikk.py
+++ Kramikk.py
@@ -188,7 +188,7 @@ class KramikkMod(loader.Module):
                                 await message.reply('отправить аптечки 10')
                             else:
                                 await message.reply(f'отправить аптечки {apt}')
-            if "pot" in message.message:
+            if "muv" in message.message:
                 if "6" in message.message:
                     a = "666 дуэлька"
                 if "d" in message.message:
