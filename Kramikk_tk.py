140d0b112acfb15a72bfd7363156dc0213f9186c
Fopregh <hifund@yandex.ru>
2021-12-21 15:23:16 +0600

    Update

--- Kramikk_tk.py
+++ Kramikk_tk.py
@@ -535,7 +535,7 @@ class KramikkMod(loader.Module):
             args = message.message
             mmsg = args.split(" ", 2)[2]
             ch = await ch
-            async for m in client.iter_messages(1521550234, search="mmsg"):
+            async for m in self.client.iter_messages(1521550234, search="mmsg"):
                 await utils.answer(ph, f'в чате {ch.title}\n\n{m.text}')
         elif (
             message.message.lower().startswith("мой клан")
