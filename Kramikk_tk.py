8df2cd2909b6fd12c008d128ce328d1472b0d1fa
kramiik <hifund@yandex.com>
2021-12-22 13:29:39 +0600

    ...

--- Kramikk_tk.py
+++ Kramikk_tk.py
@@ -575,7 +575,7 @@ class KramikkMod(loader.Module):
                     ).group(1)
                     usil = re.search(
                         "Усилитель: (.+)", response.text
-                    ).group(1)/
+                    ).group(1)
                     info = f"Чат: {ch.title}\nИмя: {message.sender.first_name}\nКлан: {klan}\nЛига: {liga}\nУсилитель: {usil}"
                     return await self.client.send_message(OPPY, info)
                 else:
