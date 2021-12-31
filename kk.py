461cc8f1ed7c5ec470b461a29dd212630a57e4c7
kramiikk <hifund@yandex.com>
2021-12-31 17:28:04 +0600

    ...

--- kk.py
+++ kk.py
@@ -127,7 +127,7 @@ class kramiikkMod(loader.Module):
                         txt += f"\nЧат: <i>{ch.title}</i>"
                         await utils.answer(nm, txt)
                         src = (
-                            f"Chat id: {chat}\n {message.sender_id} Клан:"
+                            f"Chat id: {chat} {message.sender_id} Клан:"
                         )
                         ms = await self.client.get_messages(1655814348, search=src)
                         if ms.total == 0:
