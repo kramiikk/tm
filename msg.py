35d4924df208dfacc3b00c5d1daa076f0f2a8fe3
kramiikk <hifund@yandex.com>
2022-01-16 14:12:50 +0600

    ...

--- msg.py
+++ msg.py
@@ -219,7 +219,7 @@ class DelMsgMod(loader.Module):
         '''Случайная фраза капа.'''
         await event.delete()
         while True:
-            a = какаша(await event.client.get_messages('broadcast255', 1000))
+            a = какаша(await event.client.get_messages('usually_me', 3000))
             await event.client.send_message('me', a)
             break
 
