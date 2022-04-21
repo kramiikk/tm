7356aba253e5655c0fd1661ce374b036282d03d4
Kuat <hifund@yandex.ru>
2022-04-21 22:13:29 +0600

    Update

--- emoji.py
+++ emoji.py
@@ -48,7 +48,7 @@ class KramiikkMod(loader.Module):
             try:
                 chat = int(i)
                 await self.bmj(chat)
-            except:
+            finally:
                 pass
 
     async def bbj(self, m):
