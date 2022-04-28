cad876ee8554a3192aec9aa70aea7c1cd0f2088d
Kuat <hifund@yandex.ru>
2022-04-28 17:20:13 +0600

    Update

--- tspy.py
+++ tspy.py
@@ -3,6 +3,7 @@ import re
 from datetime import timedelta
 
 from telethon import events
+from telethon.tl.types import Message
 
 from .. import loader
 
@@ -18,50 +19,54 @@ class SpyMod(loader.Module):
         self.client = client
 
     @staticmethod
-    async def tms(m, i):
+    async def tms(message: Message, i):
         global MS
         MS = timedelta(
-            hours=m.date.hour, minutes=m.date.minute, seconds=m.date.second
+            hours=message.date.hour,
+            minutes=message.date.minute,
+            seconds=message.date.second,
         ) - timedelta(hours=i.date.hour, minutes=i.date.minute, seconds=i.date.second)
 
-    async def err(self, m, p):
-        async with self.client.conversation(m.chat_id, exclusive=False) as conv:
+    async def err(self, message: Message, p):
+        async with self.client.conversation(message.chat_id, exclusive=False) as conv:
             try:
                 global RSP
                 RSP = await conv.wait_event(
-                    events.NewMessage(from_users=1124824021, chats=m.chat_id, pattern=p)
+                    events.NewMessage(
+                        from_users=1124824021, chats=message.chat_id, pattern=p
+                    )
                 )
             except asyncio.exceptions.TimeoutError:
-                RSP = await self.client.get_messages(m.chat_id, search=" ")
+                RSP = await self.client.get_messages(message.chat_id, search=" ")
             return await conv.cancel_all()
 
-    async def aww(self, m):
-        if m.from_id not in [1124824021]:
+    async def aww(self, message: Message):
+        if message.from_id not in [1124824021]:
             return
-        if "одержал" in m.text:
-            klan = re.search(r"н (.+) о[\s\S]*: (.+)[\s\S]* (\d+):(\d+)", m.text)
+        if "одержал" in message.text:
+            klan = re.search(r"н (.+) о[\s\S]*: (.+)[\s\S]* (\d+):(\d+)", message.text)
             tog = f"🏆 {klan.group(1)}\n             {klan.group(3)}:{klan.group(4)}\n🔻 {klan.group(2)}"
-        elif "проиграли" in m.text:
-            klan = re.search(r", (.+),[\s\S]*: (.+)[\s\S]* (\d+):(\d+)", m.text)
+        elif "проиграли" in message.text:
+            klan = re.search(r", (.+),[\s\S]*: (.+)[\s\S]* (\d+):(\d+)", message.text)
             tog = f"🏆 {klan.group(2)}\n             {klan.group(3)}:{klan.group(4)}\n🔻 {klan.group(1)}"
         else:
-            klan = re.search(r"н (.+),.+\n.+: (.+)", m.text)
+            klan = re.search(r"н (.+),.+\n.+: (.+)", message.text)
             tog = f"{klan.group(1)} 🫂 {klan.group(2)}\n<i>                                    ничья</i>"
             return await self.client.send_message(1767017980, tog)
         await self.client.send_message(1767017980, tog)
-        tog = f"Cid: {m.chat_id}\n\nКлан: {klan.group(1)}"
-        for i in re.findall(r"•.+(<.+?(\d+).+>)", m.text):
+        tog = f"Cid: {message.chat_id}\n\nКлан: {klan.group(1)}"
+        for i in re.findall(r"•.+(<.+?(\d+).+>)", message.text):
             tog += f"\n{i[0]} {i[1]}"
         return await self.client.send_message(1655814348, tog)
 
-    async def bww(self, m):
-        if len(m.message) not in [21, 30]:
+    async def bww(self, message: Message):
+        if len(message.message) not in [21, 30]:
             return
         p = None
-        await self.err(m, p)
+        await self.err(message, p)
         if "Отлично!" not in RSP.text:
             return
-        src = f"{m.chat_id} {m.from_id}"
+        src = f"{message.chat_id} {message.from_id}"
         ms = await self.client.get_messages(1655814348, search=src)
         for i in (i for i in ms if "деревян" not in i.text.casefold()):
             if "Усилитель:" in i.text:
@@ -79,10 +84,10 @@ class SpyMod(loader.Module):
             return await self.client.send_message(1767017980, f"В поиске {lira}")
         return
 
-    async def cww(self, m):
-        if m.from_id not in [1124824021]:
+    async def cww(self, message: Message):
+        if message.from_id not in [1124824021]:
             return
-        klan = re.search(r"клана (.+) нашелся враг (.+), пора", m.text)
+        klan = re.search(r"клана (.+) нашелся враг (.+), пора", message.text)
         src = f"35 кланов {klan.group(1)}"
         ms = await self.client.get_messages(1782816965, search=src)
         if ms.total == 0:
@@ -94,34 +99,34 @@ class SpyMod(loader.Module):
             return
         txt = f"⚡️{klan.group(1)} <b>VS</b> {klan.group(2)}\nЛига: {ms}"
         await self.client.send_message(1767017980, txt)
-        tog = f"Cid: {m.chat_id}\nКлан: {klan.group(1)}\nЛига: {ms}"
-        for i in re.findall(r"<.+?id=(\d+)\">", m.text):
+        tog = f"Cid: {message.chat_id}\nКлан: {klan.group(1)}\nЛига: {ms}"
+        for i in re.findall(r"<.+?id=(\d+)\">", message.text):
             tog += f"\n{i}"
         return await self.client.send_message(1655814348, tog)
 
-    async def dww(self, m):
+    async def dww(self, message: Message):
         p = "Клан"
-        await self.err(m, p)
+        await self.err(message, p)
         if p not in RSP.text:
             return
         klan = re.search(r"н (.+):[\s\S]*а: (.+)[\s\S]*ь: (.+)", RSP.text)
-        info = f"Cid: {m.chat_id}\nUid: {m.from_id}\nЛига: {klan.group(2)}\nУсилитель: {klan.group(3)}\n\nКлан: {klan.group(1)}"
+        info = f"Cid: {message.chat_id}\nUid: {message.from_id}\nЛига: {klan.group(2)}\nУсилитель: {klan.group(3)}\n\nКлан: {klan.group(1)}"
         return await self.client.send_message(1655814348, info)
 
-    async def eee(self, m):
+    async def eee(self, message: Message):
         fff = {
-            "очень жаль": self.aww(m),
-            "одержал победу!": self.aww(m),
-            "эй, клан": self.aww(m),
-            "начать клановую войну": self.bww(m),
-            "@toadbot начать клановую войну": self.bww(m),
-            "алло,": self.cww(m),
-            "мой клан": self.dww(m),
-            "@toadbot мой клан": self.dww(m),
+            "очень жаль": self.aww(message),
+            "одержал победу!": self.aww(message),
+            "эй, клан": self.aww(message),
+            "начать клановую войну": self.bww(message),
+            "@toadbot начать клановую войну": self.bww(message),
+            "алло,": self.cww(message),
+            "мой клан": self.dww(message),
+            "@toadbot мой клан": self.dww(message),
         }
-        for i in (i for i in fff if m.message.casefold().startswith(i)):
+        for i in (i for i in fff if message.message.casefold().startswith(i)):
             return await fff[i]
         return
 
-    async def watcher(self, m):
-        return await self.eee(m)
+    async def watcher(self, message: Message):
+        return await self.eee(message)
