e19c3d7f5db798ef77b1f4b219048b6d76776b01
Kuat <91001207+kramiikk@users.noreply.github.com>
2022-04-30 08:04:42 +0600

    update cio.py

--- cio.py
+++ cio.py
@@ -64,7 +64,7 @@ class SpyMod(loader.Module):
         await self.err(message, p)
         if 'Пойти' not in RSP.text:
             return
-        ms = await self.client.get_messages(1655814348, search="отправиться за картой")
+        ms = await self.client.get_messages(message.chat_id, search="отправиться за картой")
         txt = "Крутой клан — крутой лид!"
         for i in (i for i in ms if message.from_id not in [1124824021] and MS > timedelta(days=0, hours=8)):
             txt = 'За картой идите!'
