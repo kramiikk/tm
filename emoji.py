4233605679d04c629019a739d8d8d8b1f7724206
Kuat <hifund@yandex.ru>
2022-05-06 16:09:58 +0600

    upd

--- emoji.py
+++ emoji.py
@@ -10,7 +10,7 @@ from .. import loader
 
 @loader.tds
 class KramiikkMod(loader.Module):
-    """Алина, я люблю тебя!3.26"""
+    """Алина, я люблю тебя!4.09"""
 
     strings = {"name": "Kramiikk"}
 
