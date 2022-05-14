c188dd2b48bbf216a639743df8830f54e0061431
Kuat <hifund@yandex.ru>
2022-05-14 17:45:00 +0600

    Update

--- emoji.py
+++ emoji.py
@@ -148,10 +148,7 @@ class KramiikkMod(loader.Module):
                     )
                 except Exception:
                     continue
-                if (
-                    timedelta(days=0) == ts < timedelta(hours=3)
-                    or timedelta(days=0, hours=0) == ts
-                ):
+                if timedelta(days=0) <= ts < timedelta(hours=3):
                     continue
                 cmn = "/my_toad"
                 await self.err(chat, cmn)
