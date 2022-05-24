e8a1ad53ca7d99a315d4b29d1733af68da815dea
Kuat <hifund@yandex.ru>
2022-05-24 15:14:40 +0600

    Update

--- emoji.py
+++ emoji.py
@@ -405,5 +405,7 @@ class KramiikkMod(loader.Module):
                     return await m.reply(self.ded[msg])
                 await asyncio.sleep(random.randint(13, 33))
                 await m.respond(self.ded[msg])
+        elif m.sender_id in [830605725] and not m.text.endwith("👑🐝"):
+            await m.click()
         else:
             return
