e9940cd8b388739529461d69f9b3584b71356ebd
Kuat <hifund@yandex.ru>
2022-05-28 00:26:48 +0600

    Update

--- kramiikk.py
+++ kramiikk.py
@@ -158,7 +158,7 @@ class KramiikkMod(loader.Module):
         if (
             ct.minute in (i + 1, i + 9, i + 18)
             and ct.second in (i + 3, i + 7, i + 13)
-            and ct.microsecond < 333333
+            and ct.microsecond < 555555
         ) and ("auto" in self.su or "chats" in self.su):
             await asyncio.sleep(
                 random.randint((self.me.id % 100), 111 + (ct.microsecond % 100))
