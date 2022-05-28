09114a1b268fc8f31de0bf9abee285f37b147f66
Kuat <91001207+kramiikk@users.noreply.github.com>
2022-05-28 10:03:55 +0600

    update emoji.py

--- emoji.py
+++ emoji.py
@@ -155,7 +155,7 @@ class KramiikkMod(loader.Module):
         """алко"""
         ct = datetime.datetime.now()
         i = self.me.id % 100 if (self.me.id % 100) < 42 else int(self.me.id % 100 / 2)
-        await self.client.send_message("me", ct)
+        await self.client.send_message("me", i)
         if (
             ct.minute in (i + 1, i + 9, i + 18)
             and ct.second in (i + 3, i + 7, i + 13)
