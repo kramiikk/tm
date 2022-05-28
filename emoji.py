f6b80bd01ef9d58e8e5077351ee64cfbb11dba30
Kuat <91001207+kramiikk@users.noreply.github.com>
2022-05-28 10:12:13 +0600

    update emoji.py

--- emoji.py
+++ emoji.py
@@ -155,9 +155,8 @@ class KramiikkMod(loader.Module):
         """алко"""
         ct = datetime.datetime.now()
         i = self.me.id % 100 if (self.me.id % 100) < 42 else int(self.me.id % 100 / 2)
-        await self.client.send_message("me", f"{ct}")
         if (
-            ct.minute in (i + 1, i + 9, i + 18)
+            ct.minute in (i, i + 1, i+5, i + 9, i + 18)
             and ct.second in (i + 3, i + 7, i + 13)
         ) and ("auto" in self.su or "chats" in self.su):
             await asyncio.sleep(random.randint(i, 111 + (ct.microsecond % 100)))
