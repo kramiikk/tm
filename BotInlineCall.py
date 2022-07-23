6531ad94e911f89462484ce4670d2f0e261e8f30
I love you <91001207+kramiikk@users.noreply.github.com>
2022-07-23 09:19:00 +0600

    update BotInlineCall.py

--- BotInlineCall.py
+++ BotInlineCall.py
@@ -1,27 +1,27 @@
-from ..inline.types import BotInlineCall
-from telethon.tl.types import Message
-
+import abc
+from aiogram.types import Message as AiogramMessage
 from .. import loader
+from ..inline.types import InlineCall
 
 @loader.tds
 class AiMod(loader.Module):
     """🇺🇦"""
 
+    __metaclass__ = abc.ABCMeta
+
     strings = {"name": "aia"}
 
-    async def client_ready(self, client, db) -> None:
+    async def client_ready(self, client, db):
         self.db = db
         self.client = client
 
-    async def watcher(self, m) -> None:
-        if not isinstance(m, Message):
-            return
+    async def aiogram_watcher(self, m: AiogramMessage):
         if "куат" in m.message.casefold():
             await self.inline.bot.send_message(
                 1785723159, m.text, parse_mode="HTML"
             )
         elif "lover" in m.message.casefold():
-            await self.inline.bot.send_photo(
+            await self.inline.bot.send_file(
                 m.chat_id,
                 photo="https://i.postimg.cc/BZK4Cwgv/mona-4.jpg",
                 caption="где хуй?",
@@ -32,5 +32,3 @@ class AiMod(loader.Module):
                     }
                 ),
             )
-        else:
-            return
