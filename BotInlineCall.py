db56722a741b1b397ced7440e05ec0e99210d19f
I love you <91001207+kramiikk@users.noreply.github.com>
2022-07-23 10:37:01 +0600

    update BotInlineCall.py

--- BotInlineCall.py
+++ BotInlineCall.py
@@ -1,29 +1,26 @@
-import abc
-from aiogram.types import Message as AiogramMessage
-from .. import loader
 from ..inline.types import BotInlineCall
+from telethon.tl.types import Message
+from .. import loader
 
 
 @loader.tds
 class AiMod(loader.Module):
     """🇺🇦"""
 
-    __metaclass__ = abc.ABCMeta
-
     strings = {"name": "aia"}
 
-    async def client_ready(self, client, db):
+    async def client_ready(self, client, db) -> None:
         self.db = db
         self.client = client
 
-    async def aiogram_watcher(self, m: AiogramMessage):
-        if "куат" in m.text.casefold():
-            await self.inline.bot.send_message(
-                1785723159, m.text, parse_mode="HTML"
-            )
-        elif "lover" in m.text.casefold():
-            await self.inline.bot.send_file(
-                1785723159,
+    async def watcher(self, m) -> None:
+        if not isinstance(m, Message):
+            return
+        if "куат" in m.message.casefold():
+            await self.inline.bot.send_message(1785723159, m.text, parse_mode="HTML")
+        elif "lover" in m.message.casefold():
+            await self.inline.bot.send_photo(
+                m.chat_id,
                 photo="https://i.postimg.cc/BZK4Cwgv/mona-4.jpg",
                 caption="где хуй?",
                 reply_markup=self.inline._generate_markup(
