f14a3e2529da48f5d046f273940ebe59efc8a453
kramiikk <hifund@yandex.ru>
2022-10-12 21:28:16 +0600

    New

--- asyncio.py
+++ asyncio.py
@@ -1,3 +1,5 @@
+import asyncio
+import random
 from .. import loader, utils
 from telethon.tl.types import Message
 
@@ -6,15 +8,14 @@ from telethon.tl.types import Message
 class IsMod(loader.Module):
     """go"""
 
-    strings = {
-        "name": "Is"
-    }
+    strings = {"name": "Is"}
 
     async def client_ready(self, client, db):
         self.client = client
         self.db = db
 
     async def watcher(self, m: Message):
-        if not isinstance(m, Message) or m.from_id != -1001605756650:
+        if not isinstance(m, Message) or m.from_id != -1001460342347:
             return
-        await m.reply("pon")
+        await asyncio.sleep(random.randint(0.5, 1))
+        await m.reply("Пон")
