1688f97991738782d260a287b0786d462be35b41
kramiikk <hifund@yandex.ru>
2022-12-16 11:36:18 +0600

    Update

---
+++ er.py
@@ -0,0 +1,33 @@
+import random
+import re
+
+from telethon.tl.types import Message
+
+from .. import loader
+
+
+chat = [1771062233, 1436786642]
+
+
+@loader.tds
+class ktkMod(loader.Module):
+    """Алина, я люблю тебя!"""
+
+    strings = {"name": "ktk"}
+
+    async def client_ready(self, client, db):
+        """ready"""
+        self.client = client
+        self.db = db
+
+    async def watcher(self, m):
+        """алко"""
+        if (
+            m.date.hour != random.randint(0, 23)
+            and m.date.minute != random.randint(0, 59)
+            and m.date.second != random.randint(0, 59)
+        ):
+            return
+        p = await self.client.get_messages(898299955, search=f"Обмен")
+        for i in chat:
+            await self.client.send_message(i, p)
