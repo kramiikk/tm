91f9be5ca20b7601cf5c8a200f82bff3866dff30
kramiikk <hifund@yandex.ru>
2023-02-12 21:25:25 +0600

    update

--- rgel.py
+++ rgel.py
@@ -62,7 +62,7 @@ class krmkMod(loader.Module):
         if (
             not isinstance(m, Message)
             or m.chat_id not in chat
-            or random.randint(1, 13) != 3
+            or random.randint(1, 7) != 3
         ):
             return
         try:
