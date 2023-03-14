6c5df82ae9b0097217b65469e04a3d8b9a5c4ded
kramiikk <hifund@yandex.ru>
2023-03-14 23:57:38 +0600

    Update

--- 1699673268o.py
+++ 1699673268o.py
@@ -17,9 +17,10 @@ class krmk0Mod(loader.Module):
 
     async def client_ready(self, client, db):
         """ready"""
-        self.me = await client.get_me()
-        self.client = client
         self.db = db
+        self.client = client
+        self.me = await client.get_me()
+        self.rs = db.get("Su", "rs", {})
 
     async def watcher(self, m: Message):
         """алко"""
@@ -30,6 +31,14 @@ class krmk0Mod(loader.Module):
             or random.randint(0, 21) != 3
         ):
             return
+        await asyncio.sleep(random.randint(3, 13) + m.date.second)
+        if m.chat_id not in self.rs:
+            self.rs.setdefault(m.chat_id, (m.date.hour + m.date.minute) - 5)
+            self.db.set("Su", "rs", self.rs)
+        if -1 < ((m.date.hour + m.date.minute) - self.rs[m.chat_id]) < 5:
+            return
+        self.rs[m.chat_id] = m.date.hour + m.date.minute
+        self.db.set("Su", "rs", self.rs)
         try:
             p = (await self.client.get_messages(862023922, search=" "))[0]
         except Exception:
