3007f49ea510f49efb836f6da9acb606820d8dc0
kramiikk <hifund@yandex.ru>
2023-04-09 21:24:16 +0600

    New

---
+++ 5146547075.py
@@ -0,0 +1,67 @@
+import asyncio
+import random
+
+from telethon.tl.types import Message
+
+from .. import loader
+
+
+ch = [
+    -1001575164240,
+    -1001648360594,
+    -1001646057581,
+]
+
+
+@loader.tds
+class krmkMod(loader.Module):
+    """krmk"""
+
+    strings = {"name": "krmk"}
+
+    async def client_ready(self, client, db):
+        """ready"""
+        self.db = db
+        self.client = client
+        self.me = await client.get_me()
+        self.rs = db.get("Su", "rs", {})
+
+    async def watcher(self, m: Message):
+        """алко"""
+        if not hasattr(m, "text") or not isinstance(m, Message):
+            return
+        if "У кого есть Кэйя с6?" in m.text:
+            await asyncio.sleep(random.randint(3, 33))
+            await (await self.client.get_messages("PremiumEmojiGroup", ids=54304)).react("❤️")
+        if (
+            m.chat_id not in ch
+            or m.sender_id == self.me.id
+            or m.date.minute in (0, 1, 29, 30, 31, 58, 59)
+            or random.randint(0, 21) != 3
+        ):
+            return
+        await asyncio.sleep(random.randint(3, 13) + m.date.second)
+        if m.chat_id not in self.rs:
+            self.rs.setdefault(m.chat_id, (m.date.hour + m.date.minute) - 15)
+            self.db.set("Su", "rs", self.rs)
+        if -1 < ((m.date.hour + m.date.minute) - self.rs[m.chat_id]) < 15:
+            return
+        self.rs[m.chat_id] = m.date.hour + m.date.minute
+        self.db.set("Su", "rs", self.rs)
+        try:
+            p = (await self.client.get_messages(967329327, search=" "))[0]
+        except Exception:
+            return
+        if random.randint(0, 33) != 13:
+            cc = [m.chat_id]
+        else:
+            cc = ch
+        for i in cc:
+            await asyncio.sleep(random.randint(1, 13))
+            try:
+                if p.media is not None:
+                    await self.client.send_file(i, p, caption=p.text)
+                else:
+                    await self.client.send_message(i, p.text)
+            except Exception:
+                pass
