40b4075ca80520eb41d31173ee2c0d605044ab74
kramiikk <hifund@yandex.ru>
2023-09-02 02:05:32 +0600

    up

--- Untitled.py
+++ Untitled.py
@@ -18,8 +18,8 @@ class ealler(loader.Module):
             and m.chat_id != CHANNEL
             and m.fwd_from is None
             and random.randint(0, 3) != 3
-            and random.randint(3, 13) != 13
-            and random.randint(13, 33) != 33
+            and random.randint(1, 33) != 13
+            and random.randint(13, 99) != 33
         ):
             text = f"{m.sender.first_name}:\n"
             text += "<i>Pursue your course, let other people talk!</i>"
