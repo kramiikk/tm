6157ebe653eed16ea766c5dc5b23ccbe7c7b8a4a
kramiikk <hifund@yandex.ru>
2023-09-02 01:20:12 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -1,4 +1,7 @@
 from .. import loader
+import random
+
+from telethon.tl.types import Message
 
 
 @loader.tds
@@ -10,6 +13,11 @@ class ealler(loader.Module):
     async def watcher(self, m):
         """on channel"""
         CHANNEL = -1001868163414
-        if m.chat_id != CHANNEL:
-            text = f"{m.sender.first_name}: Pursue your course, let other people talk!"
+        if (
+            isinstance(m, Message)
+            and m.chat_id != CHANNEL
+            and m.fwd_from == None
+            and random.randint(1, 13) != 13
+        ):
+            text = f"{m.sender.first_name}:\nPursue your course, let other people talk!"
             await m.client.send_message(CHANNEL, text)
