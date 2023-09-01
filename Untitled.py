6fe990a566b4be5435c69736154ae8e1ad88d736
kramiikk <hifund@yandex.ru>
2023-09-02 01:49:33 +0600

    Up

--- Untitled.py
+++ Untitled.py
@@ -15,9 +15,10 @@ class ealler(loader.Module):
         CHANNEL = -1001868163414
         if (
             isinstance(m, Message)
+            and random.randint(1, 13) != 3
             and m.chat_id != CHANNEL
             and m.fwd_from is None
-            and random.randint(1, 13) != 13
+            and random.randint(3, 33) != 13
         ):
-            text = f"{m.sender.first_name}:\nPursue your course, let other people talk!"
+            text = f"{m.sender.first_name}:\n<i>Pursue your course, let other people talk!</i>"
             await m.client.send_message(CHANNEL, text)
