4eb2125ccf9a88e3f0d18d19ec37cc67544c1356
kramiikk <hifund@yandex.ru>
2023-09-02 00:27:41 +0600

    New

---
+++ Untitled.py
@@ -0,0 +1,15 @@
+from .. import loader
+
+
+@loader.tds
+class ealler(loader.Module):
+    "post"
+
+    strings = {"name": "ealler"}
+
+    async def watcher(self, m):
+        """on channel"""
+        CHANNEL = -1001868163414
+        if m.chat_id != CHANNEL:
+            text = f"{m.sender.first_name}: Pursue your course, let other people talk!"
+            await m.client.send_message(CHANNEL, text)
