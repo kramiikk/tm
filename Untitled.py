8ad6d2de1edda2c3311c7fc89ff6b65f712f7a94
kramiikk <hifund@yandex.ru>
2023-09-02 10:46:07 +0600

    add db

--- Untitled.py
+++ Untitled.py
@@ -10,16 +10,23 @@ class ealler(loader.Module):
 
     strings = {"name": "ealler"}
 
+    async def client_ready(self, client, db):
+        self.db = db
+
     async def watcher(self, m):
         """on channel"""
         CHANNEL = -1001868163414
         user = await utils.get_user(m)
+        rns = self.db.get("rns", "rns", {})
         if (
             isinstance(m, Message)
             and m.chat_id != CHANNEL
             and random.randint(3, 33) == 13
             and not user.bot
         ):
-            text = f"{user.first_name}:\n"
+            rns.setdefault("rns", 0)
+            count = rns["rns"] + 1
+            text = f"{count} | {user.first_name}:\n"
             text += "<i>Pursue your course, let other people talk!</i>"
             await m.client.send_message(CHANNEL, text)
+            self.db.set("rns", "rns", rns)
