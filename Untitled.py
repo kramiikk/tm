5f52dce72426426f928266872c8b2e940c5d0feb
kramiikk <hifund@yandex.ru>
2023-09-05 20:55:05 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -16,15 +16,6 @@ class ealler(loader.Module):
         self.rns.setdefault("txt", "text")
         self.rns.setdefault("rns", 0)
 
-    @staticmethod
-    async def jaccard(a: str, b: str):
-        """Calculate the Jaccard similarity between two strings"""
-        a = set(a.split())
-        b = set(b.split())
-        shared = len(a & b)
-        union = len(a | b)
-        return shared / union
-
     async def watcher(self, m):
         """on channel"""
         CHANNEL = -1001868163414
@@ -41,3 +32,10 @@ class ealler(loader.Module):
         )
         self.rns["txt"] = m.raw_text
         self.db.set("rns", "rns", self.rns)
+
+    @staticmethod
+    async def jaccard(a: str, b: str):
+        """Calculate the Jaccard similarity between two strings"""
+        a = set(a.split())
+        b = set(b.split())
+        return len(a & b) / len(a | b)
