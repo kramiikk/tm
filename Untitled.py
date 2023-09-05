703a2c52154f50fca2cc2bfeae3f510f7f184801
kramiikk <hifund@yandex.ru>
2023-09-05 19:07:09 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -12,8 +12,9 @@ class ealler(loader.Module):
 
     async def client_ready(self, client, db):
         self.db = db
-        self.rns = self.db.get("rns", "rns", 0)
-        self.txt = self.db.get("rns", "txt", "text")
+        self.rns = self.db.get("rns", "rns", {})
+        self.rns.setdefault("txt", "text")
+        self.rns.setdefault("rns", 0)
 
     async def watcher(self, m):
         """on channel"""
@@ -27,11 +28,7 @@ class ealler(loader.Module):
         user = await self.client.get_entity(m.sender_id)
         if user.bot:
             return
-        if m.text not in self.txt["txt"]:
-            self.txt["txt"] = m.text
-            self.db.set("rns", "txt", self.txt)
-        else:
-            return
+        self.rns["txt"] = m.text
         self.rns["rns"] += 1
         self.db.set("rns", "rns", self.rns)
         await self.client.send_message(
@@ -39,4 +36,4 @@ class ealler(loader.Module):
             "<i>Pursue your course, let other people talk!</i>\n"
             + f"{self.rns['rns']} | {user.first_name}",
         )
-        await self.client.send_message("me", self.txt["txt"])
+        await self.client.send_message("me", self.rns["txt"])
