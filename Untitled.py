694279c4b48d1aeccef6ec8c057ab66587b890b1
kramiikk <hifund@yandex.ru>
2023-09-05 16:37:38 +0600

    del loader import

--- Untitled.py
+++ Untitled.py
@@ -1,18 +1,17 @@
-from .. import loader, utils
 import random
 
 from telethon.tl.types import Message
 
 
 @loader.tds
-class ealler(loader.Module):
+class Ealler(loader.Module):
     """post"""
 
     strings = {"name": "ealler"}
 
     async def client_ready(self, client, db):
         self.db = db
-        self.rns = self.db.get("rns", "rns", 0)
+        self.rns = await db.get("rns", "rns", 0)
 
     async def watcher(self, m):
         """on channel"""
@@ -23,11 +22,12 @@ class ealler(loader.Module):
             or random.random() > 1 / 13
         ):
             return
-        user = await utils.get_user(m)
+        user = await self.client.get_entity(m.sender_id)
         if user.bot or random.random() < 1 / 3:
             return
-        self.rns["rns"] += 1
-        self.db.set("rns", "rns", self.rns)
-        text = "<i>Pursue your course, let other people talk!</i>\n"
-        text += str(self.rns["rns"]) + " | " + user.first_name
-        await m.client.send_message(CHANNEL, text)
+        self.rns += 1
+        await self.client.send_message(
+            CHANNEL,
+            "<i>Pursue your course, let other people talk!</i>\n"
+            + f"{self.rns} | {user.first_name}",
+        )
