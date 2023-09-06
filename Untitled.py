19943572828dcc60b77f9496a6c1381f0745258f
kramiikk <hifund@yandex.ru>
2023-09-07 01:04:25 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -9,7 +9,7 @@ class ealler(loader.Module):
 
     async def watcher(self, m):
         """channel"""
-        if m:
+        if m and not m.photo:
             i = (await self.client.get_messages(1868163414, limit=1))[0].id
             txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
             await self.client.send_message(1868163414, txt)
