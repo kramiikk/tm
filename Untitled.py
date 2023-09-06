40b5e14ba94829c6cf72aeccc6b99407031ea37a
kramiikk <hifund@yandex.ru>
2023-09-07 00:38:14 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -10,8 +10,7 @@ class ealler(loader.Module):
 
     async def watcher(self, m):
         """channel"""
-        if not isinstance(m, Message):
-            return
-        i = (await self.client.get_messages(1868163414, limit=1))[0].id
-        txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
-        await self.client.send_message(1868163414, txt)
+        if isinstance(m, Message):
+            i = (await self.client.get_messages(1868163414, limit=1))[0].id
+            txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
+            await self.client.send_message(1868163414, txt)
