fdf73d9e22d5b871713343854708253195153d8d
kramiikk <hifund@yandex.ru>
2023-09-07 01:52:49 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -12,13 +12,12 @@ class ealler(loader.Module):
         self.db = db
         self.client = client
         self.thr = db.get("Thr", "thr", {})
-        self.thr.setdefault("count", 1002)
+        self.thr.setdefault("count", 0)
 
     async def watcher(self, m):
         """channel"""
-        if m and not m.photo:
-            i = self.thr["count"] + 1
-            self.thr["count"] = i
-            txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
-            await self.client.send_message(1868163414, txt)
+        if m and m.chat_id != -1001868163414 and not m.photo:
+            txt = f" | <i>Pursue your course, let other people talk!</i>"
+            await self.client.send_message(1868163414, str(self.thr["count"]) + txt)
+            self.thr["count"] += 1
             self.db.set("Thr", "thr", self.thr)
