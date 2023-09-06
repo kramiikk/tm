5e25bcf157acc3725a3b69f7a55e43683549add3
kramiikk <hifund@yandex.ru>
2023-09-07 00:16:40 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -8,43 +8,10 @@ class ealler(loader.Module):
 
     strings = {"name": "ealler"}
 
-    async def client_ready(self, client, db):
-        self.client = client
-        self.db = db
-        self.rns = self.db.get("Rns", "rns", {})
-        self.thr = self.db.get("Thr", "thr", {})
-        self.thr.setdefault("tmm", 0)
-        self.rns = ["text"] * 13
-
-    async def jaccard(self, a: str, b: str) -> float:
-        """Calculate the Jaccard similarity between two strings"""
-        a = set(a.split())
-        b = set(b.split())
-        if not (a or b):
-            return 0.0
-        return len(a & b) / len(a | b)
-
     async def watcher(self, m):
-        """on channel"""
+        """channel"""
         CHANNEL = -1001868163414
-        if not isinstance(m, Message) or m.chat_id == CHANNEL:
+        if not isinstance(m, Message) or m.chat_id != CHANNEL:
             return
-        user = await self.client.get_entity(m.sender_id)
-        if user.bot:
-            return
-        pairs = [(t, await self.jaccard(t, m.raw_text)) for t in self.rns]
-        max_pair = max(pairs, key=lambda p: p[1])
-        t = max_pair[0]
-        x = max_pair[1]
-        if x > 1.0:
-            self.thr["tmm"] += 1
-            self.db.set("Thr", "thr", self.thr)
-            await self.client.send_message(1825043289, t)
-            await self.client.send_message(1825043289, m.raw_text)
-            a = str(self.thr["tmm"]) + " " + str(x)
-            txt = "<i>Pursue your course, let other people talk!</i>\n" + a
-            await self.client.send_message(CHANNEL, f"{txt} | {user.first_name}")
-        else:
-            pass
-        self.rns = self.rns[1:] + [m.text]
-        self.db.set("Rns", "rns", self.rns)
+        txt = "{} | <i>Pursue your course, let other people talk!</i>"
+        await self.client.send_message(CHANNEL, txt.format(m.id))
