405d216e627596e3b35a4f6cae5af939cc7f8a96
kramiikk <hifund@yandex.ru>
2023-09-07 01:00:21 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -7,8 +7,9 @@ class ealler(loader.Module):
 
     strings = {"name": "ealler"}
 
-    async def watcher(self):
+    async def watcher(self, m):
         """channel"""
-        i = (await self.client.get_messages(1868163414, limit=1))[0].id
-        txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
-        await self.client.send_message(1868163414, txt)
+        if m:
+            i = (await self.client.get_messages(1868163414, limit=1))[0].id
+            txt = f"{i} | <i>Pursue your course, let other people talk!</i>"
+            await self.client.send_message(1868163414, txt)
