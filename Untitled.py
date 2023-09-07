fe93b937c0fcf2308a6cbf81ae8b83f4404d13e8
kramiikk <hifund@yandex.ru>
2023-09-07 23:41:15 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -8,22 +8,16 @@ class ealler(loader.Module):
 
     strings = {"name": "ealler"}
 
-    async def client_ready(self, client, db):
-        """ready"""
-        self.db = db
-        self.client = client
-        self.thr = {"count": 0, "sec": 1}
+    THR = {"count": 0, "sec": 1}
 
     @loader.ratelimit(1)
     async def watcher(self, m):
         """channel"""
         if m and m.chat_id != -1001868163414 and not m.photo:
             txt = "<i>Pursue your course, let other people talk!</i>"
-            sec = int(time.time()) % 60
-            if sec != self.thr["sec"]:
-                self.thr["sec"] = sec
-                self.thr["count"] += 1
-                self.db.set("Thr", "thr", self.thr)
+            if time.time() % 60 != self.THR["sec"]:
+                self.THR["sec"] = time.time() % 60
+                self.THR["count"] += 1
                 await self.client.send_message(
-                    1868163414, f"{self.thr['count']} | {txt}"
+                    1868163414, f"{self.THR['count']} | {txt}"
                 )
