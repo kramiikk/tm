c021474cc2bf7f91c241300db8abb343975e3db7
kramiikk <hifund@yandex.ru>
2023-09-08 08:49:59 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -8,18 +8,17 @@ class ealler(loader.Module):
 
     strings = {"name": "ealler"}
 
-    THR = 1
+    THR = 0
 
     async def watcher(self, m):
         """channel"""
         if not (m or m.chat_id == 5274754956):
             return
         txt = "<i>Pursue your course, let other people talk!</i>"
-        for _ in asyncio.while_true():
+        while True:
+            self.THR += 1
+            await asyncio.sleep(1)
             await self.client.send_message(1868163414, gtext())
 
             def gtext():
                 return f"{self.THR} | {txt}"
-
-            self.THR += 1
-            await asyncio.sleep(1)
