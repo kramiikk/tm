e55fa6446248965b6e8204e5aa870b9b98aedf5a
kramiikk <hifund@yandex.ru>
2023-09-12 23:01:17 +0600

    Update

--- Untitled.py
+++ Untitled.py
@@ -14,7 +14,7 @@ class ealler(loader.Module):
         """channel"""
         if not m or m.chat_id != 5274754956 or "int" not in m.text:
             return
-        txt = "<i>Pursue your course, let other people talk!</i>"
+        txt = "<i>Pursue your course, let people talk!</i>"
         while True:
             await asyncio.sleep(1.5)
             await self.client.send_message(1868163414, f"{self.THR} | {txt}")
