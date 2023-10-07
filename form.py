8e7428d59df8702fe863169dce0d26bf11338f50
kramiikk <hifund@yandex.ru>
2023-10-07 12:33:17 +0600

    Update

--- form.py
+++ form.py
@@ -2,14 +2,18 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 import json
 
-bot = Bot("who")
+bot = Bot("")
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton("Открыть веб страницу", web_app=WebAppInfo(url="")))
+    markup.add(
+        types.KeyboardButton(
+            "Open WebApp", web_app=WebAppInfo(url="")
+        )
+    )
     await message.answer("Click button!", reply_markup=markup)
 
 
@@ -17,7 +21,6 @@ async def start(message: types.Message):
 async def web_app(message: types.Message):
     res = json.loads(message.web_app_data.data)
     await message.answer(f'Name: {res["name"]}. Username: {res["username"]}.')
-    bot.send_message(1863720231, f"Hello!")
 
 
 executor.start_polling(dp)
