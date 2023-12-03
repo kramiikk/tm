1cc79fc34a4e17ee8b867666c6509d3c07538129
kramiikk <hifund@yandex.ru>
2023-12-04 02:47:01 +0600

    update

--- 5493237361.py
+++ 5493237361.py
@@ -101,7 +101,7 @@ class krmkMod(loader.Module):
             or m.chat_id not in self.thr["chats"]
             or m.sender_id == self.me.id
             or m.date.minute in (0, 1, 29, 30, 31, 58, 59)
-            or random.randint(0, 9) != 3
+            or random.randint(0, 3) != 3
         ):
             return
         await asyncio.sleep(random.randint(3, 13) + m.date.second)
