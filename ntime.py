4bbacce834c411e2184d79c09457fa0abf900a4e
kramiikk <hifund@yandex.ru>
2024-03-10 12:47:29 +0500

    Update

--- ntime.py
+++ ntime.py
@@ -20,7 +20,6 @@ class AutoNameUpdater(loader.Module):
     async def client_ready(self, client, db):
         self.client = client
         self.name_enabled = False
-        self.timezone_offset = 5
 
     @loader.command()
     async def autonamecmd(self, message):
@@ -28,27 +27,27 @@ class AutoNameUpdater(loader.Module):
         msg = utils.get_args(message)
         if len(msg) != 1 or "{time}" not in msg[0]:
             return await utils.answer(message, self.strings["invalid_args"])
-        self.name_enabled = True
-        self.raw_name = msg[0]
+        self.client.name_enabled = True
+        raw_name = msg[0]
         await self.allmodules.log("start_autoname")
         await utils.answer(message, self.strings["enabled_name"])
 
         try:
-            while self.name_enabled:
-                current_time = datetime.now() + timedelta(hours=self.timezone_offset)
-                name = self.raw_name.format(time=current_time.strftime("%H:%M"))
+            while self.client.name_enabled:
+                current_time = datetime.now() + timedelta(hours=5)
+                name = raw_name.format(time=current_time.strftime("%H:%M"))
                 await self.client(UpdateProfileRequest(first_name=name))
                 await asyncio.sleep(59.9)
         except Exception as e:
-            self.name_enabled = False
+            self.client.name_enabled = False
             await utils.answer(message, f"<b>Stopped due to an error: {e}</b>")
 
     @loader.command()
     async def stopautonamecmd(self, message):
         """Stops the automatic name update."""
-        if not self.name_enabled:
+        if not self.client.name_enabled:
             return await utils.answer(message, self.strings["name_not_enabled"])
-        self.name_enabled = False
+        self.client.name_enabled = False
         await self.allmodules.log("stop_autoname")
         await utils.answer(message, self.strings["disabled_name"])
         await self.client(UpdateProfileRequest(first_name="00:00"))
