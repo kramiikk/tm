0c9aeb6ee5d68cac648ef2c0328ff71fc0e27369
kramiikk <hifund@yandex.ru>
2024-06-07 11:07:49 +0500

    Up

--- form.py
+++ form.py
@@ -9,7 +9,11 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton("Open WebApp", web_app=WebAppInfo(url="")))
+    markup.add(
+        types.KeyboardButton(
+            "Open WebApp", web_app=WebAppInfo(url="")
+        )
+    )
     await message.answer("Click button!", reply_markup=markup)
 
 
@@ -17,7 +21,18 @@ async def start(message: types.Message):
 async def web_app(message: types.Message):
     res = json.loads(message.web_app_data.data)
     await message.answer(f'Thank you, {res["name"]} !')
-    await bot.send_message("", f'Name: {res["name"]}. Username: {res["username"]}.')
+
+    user_mention = f'<a href="tg://user?id={message.from_user.id}">{res["name"]}</a>'
+    message_text = f'Name: {user_mention}. Username: {res["username"]}.'
+
+    await bot.send_message("", message_text)
+
+
+@dp.message_handler()
+async def forward_to_you(message: types.Message):
+    user_mention = f'<a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>'
+    await bot.send_message("", f"Message from {user_mention}:")
+    await bot.forward_message("", message.chat.id, message.message_id)
 
 
 executor.start_polling(dp)
