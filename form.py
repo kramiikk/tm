3f966f3978833d31e8013176879a01099f437718
kramiikk <hifund@yandex.ru>
2024-06-10 22:44:21 +0500

    Up

--- form.py
+++ form.py
@@ -2,28 +2,49 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 import json
 
-bot = Bot("TOKEN")
+bot = Bot("NOKEN")
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton("Open WebApp", web_app=WebAppInfo(url="")))
-    await message.answer("Click button!", reply_markup=markup)
+    markup.add(
+        types.KeyboardButton(
+            "Open WebApp", web_app=WebAppInfo(url="https://kramiikk.github.io/tm/")
+        )
+    )
+
+    await message.answer(
+        """
+        Welcome to my bot!
+        
+        <b>How to interact with our bot:</b>
+
+        • Click the "Open WebApp" button to test web application.
+        • Send any message to this chat to send it to me.
+        """,
+        parse_mode="HTML",
+    )
 
 
 @dp.message_handler(content_types=["web_app_data"])
 async def web_app(message: types.Message):
     res = json.loads(message.web_app_data.data)
     await message.answer(f'Thank you, {res["name"]} !')
-    await bot.send_message("5032015812", f'Name: {res["name"]}. Username: {res["username"]}.')
+    await bot.send_message(
+        "5032015812", f'Name: {res["name"]}. Username: {res["username"]}.'
+    )
+
 
 @dp.message_handler(content_types=types.ContentTypes.ANY)
 async def forward_to_admin(message: types.Message):
     user_mention = f'<a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>'
-    await bot.send_message("5032015812", f"Message from {user_mention}:", parse_mode="HTML")
+    await bot.send_message(
+        "5032015812", f"Message from {user_mention}:", parse_mode="HTML"
+    )
     await bot.forward_message("5032015812", message.chat.id, message.message_id)
     await message.answer("Thank you for your feedback!")
+
 
 executor.start_polling(dp)
