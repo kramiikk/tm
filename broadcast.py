bb70fb9bd109979ac7eec54035f1139eafd126c9
I love you <91001207+kramiikk@users.noreply.github.com>
2024-11-25 14:20:45 +0500

    Update

--- broadcast.py
+++ broadcast.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict, List, Optional, Set, Tuple
+from typing import Dict, List, Optional, Set, Tuple, Union
 import asyncio
 import logging
 import random
